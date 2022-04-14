0fec28a7eda09b67102f5e75ca87057fe9075b85
SplotchyXD <Brabbi67@gmail.com>
2022-04-14 11:06:35 +0300

    Komentit koodille

--- LuokanVarausOhjelma/MainWindow.xaml.cs
+++ LuokanVarausOhjelma/MainWindow.xaml.cs
@@ -21,8 +21,11 @@ namespace LuokanVarausOhjelma
 {
     public partial class MainWindow : Window
     {
+        //Asetetaan parametrit SQL yhteyden ja kommentojen käyttöön 
         public SqlConnection con;
         public SqlCommand cmd;
+        
+        //Asetetaan kaikille luokille Count funktioit joita käytetään varauksen yhteydessä
         public int KaneliCount = 0;
         public int BittiCount = 0;
         public int StudyOCount = 0;
@@ -48,13 +51,19 @@ namespace LuokanVarausOhjelma
         public int FaktaCount = 0;
         public int OivaCount = 0;
 
+        //Värit mitä käytetään kun luokka varataan tai vapautetaan
         SolidColorBrush VarausBrush = new SolidColorBrush(Colors.Red);
         SolidColorBrush VapaaBrush = new SolidColorBrush(Colors.Green);
 
         public MainWindow()
         {
-           
-
+            //tässä kohdassa kutsutaan kaikkia methodeja joita tässä projektissa käytetään
+            /*
+             * Get_Data_SQL methodi tarkistaa kaikkien luokkien varausten tilat
+             * StartKello methodi aloittaa ohjelman sisäisin kellon toiminnon
+             * Startkellosta 4 riviä on tarkoitettu tietojen päivitykselle jotta ohjelmalla on aina tiedot oikeani(koska tätä pyöritetään aulassa), tämä suoritetään timer:ia käyttäen
+             * timer:ille on asetettu aikaväli millä se toimii(30 sekunttia), mitä tapahtuu kun timerin aika on loppu(aloitetaan uusi) ja lopussa kerrotaan aloitetaan timer
+            */
             InitializeComponent();
             Get_Data_SQL();
             StartKello();
@@ -63,20 +72,25 @@ namespace LuokanVarausOhjelma
             timer.Elapsed += timer_Elapsed;
             timer.Start();
 
+            //tätä methodia käytetään päivittämään tietoja ja tätä kutsutaan kun timerista loppuu aika ja aloitetaan uusi timer
             void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
             {
                 Dispatcher.BeginInvoke(new Action(() =>
                 {                    
                     Get_Data_SQL();
-                    MessageBox.Show("paska");
+                    MessageBox.Show("Toimii");
                 }));
-            }
-
-           
+            }   
         }
 
         private void Nappi_painettu(object sender, KeyEventArgs e)
         {
+            //Tämän methodin ideana on ottaa kaikista luokista varaus inputit(eli napin painallukset) ja sen mukaan asettaa varaukset ja vapautukset
+            //methodin alussa kerrotaan mihin tietokantaan yhteys avataaan
+            //yhteys avataan
+            //riippuen mitä nappia painetaan niin haetaan sen mukaan luokka ja katsotaan onko se varattu tai ei
+            //jos luokka on varattu niin luokka asetetaan vapaaksi ja jos se on vapaana niin se asetetaan varatuksi
+            //jokainen komento on sama jokaiselle luokalle ainoa ero on se että jos luokka on varattu niin se asetetaan vapaksi eli 0 ja jos se on varattu niin tilan numeroksi tulee 1
             con = new SqlConnection(@"Data Source = teamempiresrv.database.windows.net; Initial Catalog = LuokkaVaraus; Persist Security Info = True; User ID = Empire; Password = Nice1234");
             con.Open();
             if (e.Key == Key.Space && BittiCount == 0)
@@ -373,7 +387,8 @@ namespace LuokanVarausOhjelma
                 OivaCount--;
                 Oiva.Fill = VapaaBrush;
             }
-
+            //tässä kohdassa valittu komento suoritetaan
+            //ja sen jälkeen yhteys suljetaan((yhteyksen sulkeminen on sitä varten että kukaan jolla on tietoa sql:stä ei voi suorittaa SQL Injectionia ja päästä käsiksi tietokantaan))
             cmd.ExecuteNonQuery();
             con.Close();
         }
@@ -382,9 +397,19 @@ namespace LuokanVarausOhjelma
 
         public void Get_Data_SQL()
         {
+            //tämän methodin ideana on tarkistaa luokkien varausten tila kun ohjelma käynnistetään tai kun ohjelman tiedot päivitetään(joka 30 sekuntti)
+            //methodin alussa asetetaan SqlDataReader funktiolle rdr nimi ja se asetetaan null:iksi koskak un sitä kutsutaan niin sen pitää olla null
+            //ja kuten Nappi_painettu methodissa niin kerrotaan mihin tietokantaan ohjelma yhdistaa kun yhteytta avataan
             SqlDataReader rdr = null;
             con = new SqlConnection(@"Data Source = teamempiresrv.database.windows.net; Initial Catalog = LuokkaVaraus; Persist Security Info = True; User ID = Empire; Password = Nice1234");
 
+            //yhteys avataan
+            //asetetaan mitä komento tekee kun sitä käytetään (komento hakee Is_Varattu sarakkeen tietokannasta)
+            //komento satetaan texti muotoon jotta SqlDataReader pystyy lukemaan sen
+            //komento suoritetaan ja sen jälkeen rdr lukee mitä se on löytänyt
+            //jos rdr löytää että Is_Varattu on true(eli siis varatttu) niin asetetaan luokkka varatuksi(1) ja väriksi laitetaan VarausBrush(Punainen)
+            //jos rdr löytää että Is_Varattu on False(eli siis vapaa) niin aseteaan luokkka vapaaksi(0) ja väriksi VapaaBrush(Vihreä)
+            //ja lopussa suljetaan yhteys tietokantaan(yhteyksen sulkeminen on sitä varten että kukaan jolla on tietoa sql:stä ei voi suorittaa SQL Injectionia ja päästä käsiksi tietokantaan)
             con.Open();
             cmd = new SqlCommand("Select Is_Varattu from Luokat Where LuokkaNimi ='StudyO'", con);
             cmd.CommandType = System.Data.CommandType.Text;
@@ -795,6 +820,8 @@ namespace LuokanVarausOhjelma
 
         public void StartKello()
         {
+            //asetetaan DispatcherTimer:ille timeksi timer
+            //tämän jälkeen asetettaan timerille parametrit jotta se toimisi kuin kello
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += tickevent;
@@ -803,8 +830,8 @@ namespace LuokanVarausOhjelma
 
         private void tickevent(object sender, EventArgs e)
         {
+            //asetetaan teksti elementtin teksti kellon ajaksi 
             kellolbl.Text = DateTime.Now.ToString();
-
         }
 
     }
